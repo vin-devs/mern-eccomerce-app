faad81145d7d423e9f8fec6852b80b3d7419db18
vin-devs <vmutuku706@gmail.com>
2026-03-27 20:16:27 +0300

    Fix: Added credentials include to apiSlice

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,10 +1,14 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+// Adding credentials: "include" is what allows the JWT to travel to the backend
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  credentials: "include",
+});
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User", "Product", "Order", "Category"],
-  endpoints: () => ({}),
+  endpoints: (builder) => ({}),
 });
